0cbee82db0404c0e9645e37ffe2c95ce80af41b4
Ekk0001 <70920555+Ekk0001@users.noreply.github.com>
2020-11-30 16:32:39 -0500

    Door to Maze, Scale changes
    Corrected sixe of floorplan, finished item movements, and made openable door to maze

--- GreatHoneyHeist/Assets/Scripts/beeKeyBehavior.cs
+++ GreatHoneyHeist/Assets/Scripts/beeKeyBehavior.cs
@@ -9,13 +9,11 @@ public class beeKeyBehavior : MonoBehaviour
 
     public static bool playerHaveKey;   // Does the Player have the bee key?
 
-
     // Runs upon the object being brought into existance
     private void Awake()
     {
         playerHaveKey = false; // Player starts out without the key
     }
-
 
     // ====== COLLIDING WITH PLAYER / PLAYER PICKING UP KEY ======
     private void OnTriggerEnter(Collider col)
@@ -24,8 +22,14 @@ public class beeKeyBehavior : MonoBehaviour
         if (col.gameObject.tag == "Player")
         {
             if (debug) {Debug.Log("Player picked up key");}
-            Destroy(gameObject);
-            playerHaveKey = true;
+            this.transform.parent = col.transform;                  //Have the Key follow the player
+            playerHaveKey = true;   
+           
+        }
+        if (col.gameObject.tag == "Door")                           //If the key (Now on the player) hits the door, open the door
+        {
+            col.transform.Rotate (0,90,0);
+            Destroy(gameObject);                                    //Destroy key to avoid rapid door spinning
         }
     }
 }
